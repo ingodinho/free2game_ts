b2054d0d8d9f34cb4d60da32972a9baa2df2d2fc
ingodinho <siemens.ingo@googlemail.com>
2023-10-12 14:44:12 +0200

    React Init, Styled Components, Style Reset, Style Variables

---
+++ src/constants.ts
@@ -0,0 +1,35 @@
+const color = {
+    primary: "#FF4654",
+    text: "#fff",
+    background: "#040F14",
+    accentDark: "#102130",
+    accentLight: "#375470"
+}
+
+const fontFamily = {
+    primary: `"Oswald", sans-serif`,
+    secondary: `"Titillium", sans-serif`
+}
+
+const fontSize = {
+    16: "1rem",
+    18: "1.125rem",
+    24: "1.5rem",
+    48: "3rem",
+}
+
+const spacing = {
+    xs: "8px",
+    s: "16px",
+    m: "24px",
+    l: "36px",
+    xl: "48px",
+    xxl: "72px"
+}
+
+export const css = {
+    color,
+    fontFamily,
+    fontSize,
+    spacing
+}
